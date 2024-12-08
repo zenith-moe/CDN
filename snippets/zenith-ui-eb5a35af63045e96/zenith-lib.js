63998edf84e2eaf29963d5f5c445e462ffd34a89
KinLeoapple <66625626+KinLeoapple@users.noreply.github.com>
2024-12-09 01:04:39 +0800

    Add files via upload

--- snippets/zenith-ui-eb5a35af63045e96/zenith-lib.js
+++ snippets/zenith-ui-eb5a35af63045e96/zenith-lib.js
@@ -32,3 +32,11 @@ export function js_change_bg(class_name) {
 export function js_drop_bg(class_name) {
     document.body.classList.remove(class_name);
 }
+
+export function get_timezone() {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+}
+
+export function get_timezone_offset() {
+    return new Date().getTimezoneOffset();
+}
