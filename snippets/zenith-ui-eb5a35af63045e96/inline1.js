7b72f73f285bb5fd3cd7f31418b2e6a65f2db668
KinLeoapple <66625626+KinLeoapple@users.noreply.github.com>
2025-01-02 17:52:00 +0800

    Delete snippets/zenith-ui-eb5a35af63045e96 directory

--- snippets/zenith-ui-eb5a35af63045e96/inline1.js
+++
@@ -1,7 +0,0 @@
-export function js_change_bg(class_name) {
-    document.body.classList.add(class_name);
-}
-
-export function js_drop_bg(class_name) {
-    document.body.classList.remove(class_name);
-}
